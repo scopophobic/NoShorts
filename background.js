58475b48254970cd8bf0dec17768e40759aa8d94
scopophobic <adithyanmadhu1234@hmail.com>
2024-10-25 05:27:44 +0530

    final

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("youtube.com/shorts")) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      func: () => {
-        if (window.location.href.includes("youtube.com/shorts")) {
-          window.location.href = chrome.runtime.getURL("blocked.html");
-        }
-      }
-    });
+  if (
+    changeInfo.status === "complete" &&
+    tab.url.includes("youtube.com/shorts")
+  ) {
+    chrome.tabs.update(tabId, { url: chrome.runtime.getURL("blocked.html") });
   }
 });
